6615a9ac3cb9fea065c7bd56bf395da4fe1db9db
Muhammad Hamdan Bhatti <hamdanbutt991gmail.com>
2024-07-08 01:45:01 +0500

    New Changes

--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
-// Count Down Timer
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { differenceInSeconds } from "date-fns";
+
+const response = await inquirer.prompt([
+     {
+          name: "userInput",
+          type: "number",
+          message: "Please Enter the Amount of second",
+          validate: (input) => {
+               if (isNaN(input)) {
+                    return "Please Enter a valid number";
+               } else if (input > 60) {
+                    return "Number must be less than or equal to 60";
+               } else {
+                    return true;
+               }
+          },
+     },
+]);
+
+let input = response.userInput;
+
+function startTime(val: number) {
+     let initTime = new Date().setSeconds(new Date().getSeconds() + val);
+     const intervalTime = new Date(initTime);
+     setInterval(() => {
+          const currTime = new Date();
+          let timeDiff = differenceInSeconds(intervalTime, currTime);
+
+          if (timeDiff <= 0) {
+               console.log(chalk.red("Timer has expired"));
+               process.exit();
+          }
+          const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+          const sec = Math.floor(timeDiff % 60);
+
+          console.log(`${min.toString().padStart(2, "0")} : ${sec.toString().padStart(2, "0")}`);
+     }, 1000);
+}
+
+startTime(input);
