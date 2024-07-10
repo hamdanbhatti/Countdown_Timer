0964f06886a15d03e84efe15d8b0e09aef64efbf
Muhammad Hamdan Bhatti <hamdanbutt991gmail.com>
2024-07-10 06:01:18 +0500

    New Changes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 import { differenceInSeconds } from "date-fns";
@@ -5,13 +6,13 @@ const response = await inquirer.prompt([
     {
         name: "userInput",
         type: "number",
-        message: "Please Enter the Amount of second",
+        message: "Please Enter the Amount of seconds",
         validate: (input) => {
             if (isNaN(input)) {
                 return "Please Enter a valid number";
             }
-            else if (input > 60) {
-                return "Number must be less than or equal to 60";
+            else if (input < 0) {
+                return "Number must be greater than or equal to 0";
             }
             else {
                 return true;
@@ -21,18 +22,22 @@ const response = await inquirer.prompt([
 ]);
 let input = response.userInput;
 function startTime(val) {
+    console.log(chalk.yellow.bold.italic("\n\tStarting Timer ...\n\n"));
     let initTime = new Date().setSeconds(new Date().getSeconds() + val);
     const intervalTime = new Date(initTime);
     setInterval(() => {
         const currTime = new Date();
         let timeDiff = differenceInSeconds(intervalTime, currTime);
         if (timeDiff <= 0) {
-            console.log(chalk.red("Timer has expired"));
+            console.log(chalk.red.bold.bgYellow("\t\tTimer has expired.."));
             process.exit();
         }
-        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const hours = Math.floor(timeDiff / 3600);
+        const min = Math.floor((timeDiff % 3600) / 60);
         const sec = Math.floor(timeDiff % 60);
-        console.log(`${min.toString().padStart(2, "0")} : ${sec.toString().padStart(2, "0")}`);
+        const timerOutput = chalk.greenBright.bold(`\t\t${hours.toString().padStart(2, "0")} : ${min.toString().padStart(2, "0")} : ${sec.toString().padStart(2, "0")}`);
+        console.log(timerOutput);
+        console.log(); // Empty line for gap
     }, 1000);
 }
 startTime(input);
